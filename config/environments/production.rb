3045fe53bd8269e98025c8b0769490764335d43f
Shintaro Sekimoto <ssekimoto@example.com>
2024-10-02 14:01:43 +0000

    fix hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.hosts << /.*\.cloudworkstations\.dev/
+  config.hosts << /.*\.asia-northeast1\.run\.app/
 end
